e8bdcbd4c94ad026933f0dd5d1ff350366e70ab0
laptopmintholasik-sudo <laptopmintholasik@gmail.com>
2026-07-04 08:24:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const statusText = document.getElementById("status");
 
+const btnKamera = document.getElementById("btnKamera");
+const btnFoto = document.getElementById("btnFoto");
+const btnUlang = document.getElementById("btnUlang");
+const btnSimpan = document.getElementById("btnSimpan");
+
+btnKamera.onclick = function(){
+
+    statusText.innerHTML="🟡 Membuka kamera...";
+
+    alert("Versi 2.4 berhasil dipasang.");
+
+}
